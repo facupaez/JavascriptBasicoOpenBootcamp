a0da8bdfc12e683eed236f7e1a01406fa3321d17
facupaez <facu.pz10@gmail.com>
2023-03-05 13:17:34 -0300

    Uso de listas, fechas, objetos

---
+++ ejercicios/Ejercicio 02/index.js
@@ -0,0 +1,16 @@
+const lista = [
+  {
+    nombre: "Facundo",
+    edad: 29,
+    esDesarrollador: true,
+    fechaNacimineto: new Date(1994, 1, 15),
+    libroFavorito: {
+      titulo: "Clean Code",
+      autor: "Robert C. Martin",
+      fecha: new Date(2008, 7, 1),
+      url: "https://www.audible.com/pd/Clean-Code-Audiobook/B08X7KL3TF?source_code=UHSPP30DTRIAL548101222017X&ipRedirectOverride=true&&cvosrc=ppc%20content.googlegdn.18595012441&cvo_crid=628101289156&cvo_pid=141954718986&source_code=&gclid=CjwKCAiAmJGgBhAZEiwA1JZollgtGdWASXudF-FgVoH0bRHZw5-DfYSpU6JWcOQhevRuHOSc6a6HWhoCeqwQAvD_BwE&gclsrc=aw.ds",
+    },
+  },
+];
+
+console.log(lista);
