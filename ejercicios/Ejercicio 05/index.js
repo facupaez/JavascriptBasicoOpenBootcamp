666e75fc8977f634042c2d1b8da9068ae226c622
facupaez <facu.pz10@gmail.com>
2023-03-06 23:11:16 -0300

    métodos numéricos

---
+++ ejercicios/Ejercicio 05/index.js
@@ -0,0 +1,11 @@
+let alturaCm = 170;
+let alturaM = 1.71;
+let pesoKg = 80.3;
+let alturaRed = Math.ceil(alturaM);
+let pesoRed = Math.floor(pesoKg);
+
+console.log("La altura redondeada hacia arriba es:", alturaRed, "m");
+console.log("El peso redondeado hacia abajo es:", pesoRed, "kg");
+
+let maxNum = Number.MAX_VALUE;
+console.log(maxNum === maxNum + 1);
