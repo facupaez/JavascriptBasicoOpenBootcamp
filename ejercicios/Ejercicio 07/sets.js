a6bd193495781e4e76183161667d0ef63609c56f
facupaez <facu.pz10@gmail.com>
2023-03-11 21:59:45 -0300

    Listas Set

---
+++ ejercicios/Ejercicio 07/sets.js
@@ -0,0 +1,11 @@
+const familia = ["Carl", "John", "Lili", "Darla"];
+const familiaSet = new Set(familia);
+console.log(familiaSet);
+
+//adding same value in set
+familiaSet.add("Darla");
+console.log(familiaSet);
+
+//adding another value in set
+familiaSet.add("Javascript");
+console.log(familiaSet);
