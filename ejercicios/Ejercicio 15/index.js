99817c8c0235475bd3f9b17dde8ee4aadaf6b668
facupaez <facu.pz10@gmail.com>
2023-03-15 23:34:25 -0300

    Persistencia de datos en navegador

---
+++ ejercicios/Ejercicio 15/index.js
@@ -0,0 +1,17 @@
+let nombre = "Miguel",
+  apellido = "Sanchez";
+const misDatos = {
+  nombre,
+  apellido,
+};
+
+/* sessionStorage.setItem("misDatos", JSON.stringify(misDatos));
+console.log(JSON.parse(sessionStorage.getItem("misDatos", misDatos)));
+
+localStorage.setItem("misDatos", JSON.stringify(misDatos));
+console.log(JSON.parse(localStorage.getItem("misDatos", misDatos)));
+
+document.cookie =
+  `misDatos=${JSON.stringify(misDatos)}; expires=` +
+  new Date(Date.now() + 60 * 100).toUTCString();
+console.log(document.cookie); */
